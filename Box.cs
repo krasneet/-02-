bc1ec57e37c1d1d09849400b39e84e414e5ca42e
Choppa <150598238+krasneet@users.noreply.github.com>
2025-03-24 14:39:53 +0300

    1

---
+++ Box.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Box
+    {
+        private int l;
+        private int b;
+        private int h;
+
+         public int L
+        {
+
+            get 
+            {
+                return l;
+            }
+            set
+            {
+                if (value > 0) l = value;
+            }
+
+        }
+
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+
+            set
+            {
+                if (value > 0) b = value;
+            }
+
+        }
+
+
+        public int H
+        {
+            get
+            {
+                return h;
+            }
+            set
+            {
+                if (value > 0)
+                    h = value;
+            }
+        }
+
+        public Box()
+        {
+            L = 0;
+            B = 0;
+            H = 0;
+        }
+
+        public Box(int d, int s, int w)
+        {
+            L = d;
+            B = s;
+            H = w;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"l = {L}, b ={B}, h={H}");
+            return;
+        }
+
+        public int Volum()
+        {
+            int v = l * b * h;
+            return v;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(5,2,3);
+
+            box.Show();
+
+            int x = box.Volum();
+
+            Console.WriteLine($"объем коробки {x}");
+
+            Console.ReadKey();
+        }
+    }
+}
